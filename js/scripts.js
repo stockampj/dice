e8711cbdf4280de6953a929099a7730527df1e14
joel stockamp, kira roberts and molly stambaugh <molly@>
2019-08-28 15:00:47 -0700

    add front end logic, html, css, dice pictures

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // Business End for game
-function Game (player){
+var game = new Game();
+player1 = new Player();
+player2 = new Player();
+
+function Game() {
   this.players = []
   // this.player1 = player1,
   // this.player2 = player2
@@ -9,15 +13,24 @@ Game.prototype.addPlayer = function (player){
     this.players.push(player);
 }
 
+Game.prototype.endGame = function (){
+
+}
+
 
 // Business End for player
-function Player (name) {
-  this.name = name,
+function Player () {
+  this.name = "name"
   this.totalscore = 0,
   this.turnscore = [],
   this.rollvalue = 0,
   this.turn = true;
 }
+
+Player.prototype.addName = function (name) {
+  this.name = name;
+}
+
 
 Player.prototype.roll = function() {
   var roll = Math.floor(Math.random()*6)+1
@@ -27,6 +40,7 @@ Player.prototype.roll = function() {
   } else {
       this.turnscore.push(roll);
   }
+  return roll;
 }
 
 Player.prototype.endTurn = function() {
@@ -35,14 +49,29 @@ Player.prototype.endTurn = function() {
     turnpoints = turnpoints + val;
   });
   this.totalscore += turnpoints;
-    this.turn = false
+  this.turn = false
+  this.turnscore = [];
 }
 
-
-
-
-
 // Front End
-// $(document).ready(function() {
-//   $(" ").submit(function(event) {
-//     event.preventDefault();
+var diceClass = [" ", "dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+
+$(document).ready(function() {
+  $("#start").submit(function(event) {
+    event.preventDefault();
+    var player1name = $("#player1-name").val();
+    var player2name = $("#player2-name").val();
+    player1.addName(player1name);
+    player2.addName(player2name);
+    game.addPlayer(player1);
+    game.addPlayer(player2);
+  });
+
+  $("#roll-button1").click(function() {
+    player1.roll();
+    console.log(player1.roll());
+    var index = diceClass[player1.roll()];
+    console.log(index);
+    // $("#dice-display-1").addClass(class);
+  });
+});
