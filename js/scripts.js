51dd2076c3713cc73ae7dfd6708889b47dd8e120
joel stockamp, kira roberts and molly stambaugh <kira@>
2019-08-28 11:48:09 -0700

    fix errors in function syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 // Business End for game
-function Game (player1, player2){
-  this.player1 = player1,
-  this.player2 = player2,
+function Game (player){
+  this.players = []
+  // this.player1 = player1,
+  // this.player2 = player2
 }
 
-// Game.prototype.addNames function (name1, name2){
-//     this.player1 = name1;
-//     this.player2 = name2;
-// }
+Game.prototype.addPlayer = function (player){
+    this.players.push(player);
+}
 
 
 // Business End for player
-function Player (name)
+function Player (name) {
   this.name = name,
   this.totalscore = 0,
   this.turnscore = [],
@@ -19,7 +19,7 @@ function Player (name)
   this.turn = true;
 }
 
-Player.prototype.roll function() {
+Player.prototype.roll = function() {
   var roll = Math.floor(Math.random()*6)+1
   if (roll === 1) {
     this.turnscore = [];
@@ -29,11 +29,11 @@ Player.prototype.roll function() {
   }
 }
 
-Player.prototype.endTurn function() {
+Player.prototype.endTurn = function() {
   var turnpoints = 0;
-  this.turnscore.forEach(val) {
+  this.turnscore.forEach(function(val) {
     turnpoints = turnpoints + val;
-  }
+  });
   this.totalscore += turnpoints;
     this.turn = false
 }
@@ -43,6 +43,6 @@ Player.prototype.endTurn function() {
 
 
 // Front End
-$(document).ready(function() {
-  $(" ").submit(function(event) {
-    event.preventDefault();
+// $(document).ready(function() {
+//   $(" ").submit(function(event) {
+//     event.preventDefault();
